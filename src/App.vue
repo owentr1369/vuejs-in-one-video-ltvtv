05ca96cd2374d33ed0273b54ab0ecae9466919af
TranTam <tamvaa1306@gmail.com>
2022-06-27 11:43:11 +0700

    eventlistener

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,20 @@
     <h1>VueJS LTV TV</h1>
     <!-- <two-ways-binding /> -->
     <!-- <v-show /> -->
-    <v-if-v-else />
+    <!-- <v-if-v-else /> -->
+    <event-listener />
   </div>
 </template>
 
 <script>
 // import TwoWaysBinding from "./components/TwoWaysBinding.vue";
 // import VShow from "./components/VShow.vue";
-import VIfVElse from "./components/VIfVElse.vue";
+// import VIfVElse from "./components/VIfVElse.vue";
+import EventListener from "./components/EventListener.vue";
 
 export default {
   name: "App",
-  components: { VIfVElse },
+  components: { EventListener },
 };
 </script>
 
---
+++ src/components/EventListener.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>{{ heading }}</h1>
+    <button @mouseover="heading = 'Hi!'">Change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      heading: "Hello",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
