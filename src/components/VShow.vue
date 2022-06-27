558a014326aaa788cb130f46dd719732c3249152
TranTam <tamvaa1306@gmail.com>
2022-06-27 10:29:07 +0700

    v-show

---
+++ src/components/VShow.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1 v-show="isShow">Hello</h1>
+    <button @click="show">Show/Hide</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: false,
+    };
+  },
+  methods: {
+    show: function () {
+      this.isShow = !this.isShow;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
