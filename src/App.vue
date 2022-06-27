3825ae198e904836b03ab1a19b050b5e6d866635
TranTam <tamvaa1306@gmail.com>
2022-06-27 10:33:08 +0700

    v-if v-else

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
     <h1>VueJS LTV TV</h1>
-    <two-ways-binding />
+    <!-- <two-ways-binding /> -->
+    <!-- <v-show /> -->
+    <v-if-v-else />
   </div>
 </template>
 
 <script>
-import TwoWaysBinding from "./components/TwoWaysBinding.vue";
+// import TwoWaysBinding from "./components/TwoWaysBinding.vue";
+// import VShow from "./components/VShow.vue";
+import VIfVElse from "./components/VIfVElse.vue";
+
 export default {
   name: "App",
-  components: { TwoWaysBinding },
+  components: { VIfVElse },
 };
 </script>
 
---
+++ src/components/VIfVElse.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 v-if="show">Show</h1>
+    <h2 v-else>Hide</h2>
+    <button @click="isShow">Switch</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
+  methods: {
+    isShow: function () {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
