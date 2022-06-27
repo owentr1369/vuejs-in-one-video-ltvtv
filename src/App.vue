8bf2f7c061a067eb45d3fb483b11c432327d01de
TranTam <tamvaa1306@gmail.com>
2022-06-27 10:16:31 +0700

    twowaysbinding

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
-    <h1>Hello</h1>
+    <h1>VueJS LTV TV</h1>
+    <two-ways-binding />
   </div>
 </template>
 
 <script>
+import TwoWaysBinding from "./components/TwoWaysBinding.vue";
 export default {
   name: "App",
+  components: { TwoWaysBinding },
 };
 </script>
 
 <style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+  flex-direction: column;
+}
 </style>
---
+++ src/components/TwoWaysBinding.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <input type="text" v-model="name" />
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { name: "Tran Thien Tam" };
+  },
+};
+</script>
+
+<style>
+</style>
